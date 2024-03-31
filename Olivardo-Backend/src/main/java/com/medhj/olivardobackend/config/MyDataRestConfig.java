774ed626554bb0a70cbd7c7b807079d2415e04bd
med-HJ <63222887+med-HJ@users.noreply.github.com>
2024-03-31 00:31:22 +0000

    configure Rest API for Read only

---
+++ Olivardo-Backend/src/main/java/com/medhj/olivardobackend/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.medhj.olivardobackend.config;
+
+
+import com.medhj.olivardobackend.entity.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.lang.annotation.Annotation;
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+
+
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadate, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadate, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+}
